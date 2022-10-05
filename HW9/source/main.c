99c5e7a0db14bdffffe5b7fd8e76f75763ef1f2c
jamesdinner <godjames135790@gmail.com>
2022-10-06 00:33:58 +0800

    fix

--- HW9/source/main.c
+++ HW9/source/main.c
@@ -2,11 +2,31 @@
 
 int main()
 {
-	int a;
-	a = 200 + 300;
-	printf("一整天的總里程數=100公里\n");
-	printf("汽油一公升/300 NT\n");
-	printf("平均一公升/100 公里\n");
-	printf("一天的停車費=200");
-	printf("一天的通行費 = 停車費 + 油錢 = %d\n",a);
+	int a,b,c,d;
+	float s,g;
+	printf("加油必須以每公升為單位");
+	printf("一整天的總里程數=");
+	scanf("%d",&a);
+	printf("\n");
+	printf("汽油一公升/NT.");
+	scanf("%d", &b);
+	printf("\n");
+	printf("平均一公升/km");
+	scanf("%d", &c);
+	printf("\n");
+	printf("一天的停車費NT.");
+	scanf("%d", &d);
+	printf("\n");
+	if (a%c != 0)
+	{
+		a = a / c; 
+			a++;
+	}
+	if (a%c == 0)
+	{
+		a = a / c;
+	}
+	g = a * b;
+	s = d + g;
+	printf("一天的通行費 = 停車費 + 油錢 = %0.2f\n",s);
 }
