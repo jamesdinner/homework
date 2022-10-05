aa59212373437f336b98c658fa4fffed59e18892
jamesdinner <godjames135790@gmail.com>
2022-10-05 23:10:07 +0800

    finish

---
+++ HW9/source/main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+	int a;
+	a = 200 + 300;
+	printf("一整天的總里程數=100公里\n");
+	printf("汽油一公升/300 NT\n");
+	printf("平均一公升/100 公里\n");
+	printf("一天的停車費=200");
+	printf("一天的通行費 = 停車費 + 油錢 = %d\n",a);
+}
